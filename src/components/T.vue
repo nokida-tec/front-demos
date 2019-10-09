95de54935e8648e33fdb20e7d89b61088cf1b3f2
xu yadong <3368603146@qq.com>
2019-10-09 11:08:47 +0800

    初始化仓库 修改README

---
+++ src/components/T.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <slideBar></slideBar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import slideBar from './main/slideBar/slideBar.vue'
+
+  export default {
+    components: {
+      slideBar: slideBar
+    }
+  }
+</script>
